9588a2e1d006b4a8a821f4b06afba9a22bed31c3
debdevs <debdevs97@gmail.com>
2022-10-08 05:49:42 -0400

    revise template landing page
    testing different layouts and sizing for templates landing page. Created more responsive styling

--- src/components/UI/atoms/EditorButton/EditorButton.jsx
+++ src/components/UI/atoms/EditorButton/EditorButton.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import './EditorButton.css';
 const EditorButton = ({ button_text }) => {
   return (
-    <div>
+    <div className="editor-button-parent">
       <button class="editor-button">{button_text}</button>
     </div>
   );
