99031b9e4a9d147cec881604c1db4ce296838b04
debdevs <debdevs97@gmail.com>
2022-10-20 12:52:00 -0400

    create framer motion carousel with enter/exit
    created framer motion carousel with enter/exit animations. Inmported new carousel to TemplatesCarouselHero.

---
+++ src/components/UI/organisms/MotionCarousel/MotionCarousel.jsx
@@ -0,0 +1,88 @@
+import React from 'react';
+import { Context } from 'react';
+import { useContext } from 'react';
+import { useState } from 'react';
+import { TemplatesContext } from '../../../../Contexts/TemplatesPageContext';
+import { motion } from 'framer-motion/dist/framer-motion';
+import { AnimatePresence } from 'framer-motion/dist/framer-motion';
+import './MotionCarousel.css';
+export const CarouselItem = ({ children, width }) => {
+  const { activeIndex, setActiveIndex } = useContext(TemplatesContext);
+  const [rotation, setRotation] = useState(0);
+  const [scale, setScale] = useState(1);
+  const variants = {
+    initial: { x: 200, opacity: 0 },
+    animate: { x: 0, opacity: 0 },
+    exit: { x: 0, opacity: 0 },
+  };
+  return (
+    <AnimatePresence>
+      <motion.div
+        key={activeIndex}
+        className="carousel_parent"
+        initial={{ scaleX: 0.9, opacity: 0 }}
+        animate={{ scaleX: 1, opacity: 1 }}
+        exit={{
+          scaleX: 0.97,
+          opacity: 0,
+          transition: { type: 'spring', stiffness: 50 },
+        }}
+        transition={{ duration: 1 }}
+      >
+        <carousel-item>
+          <img src=""></img>
+          {children}
+        </carousel-item>
+      </motion.div>
+    </AnimatePresence>
+  );
+};
+
+const MotionCarousel = ({ children }) => {
+  const { activeIndex, setActiveIndex } = useContext(TemplatesContext);
+
+  const updateIndex = (newIndex) => {
+    if (newIndex < 0) {
+      newIndex = 0;
+    } else if (newIndex >= React.Children.count(children)) {
+      newIndex = React.Children.count(children) - 1;
+    }
+    setActiveIndex(newIndex);
+  };
+  return (
+    <motion-carousel className="motion-carousel">
+      <inner-content
+        className="inner-content"
+        //take number of slides. Divide item width by that number. Divide result by 1/2 (and ask google what percent of width is result)to get percantage for shifting to center 'translateX(12.5015211%)'
+        //style={{ transform: `translateX(-${activeIndex * 12.5015211}% )` }}
+        style={
+          {
+            // transform: `translateX(-${activeIndex * 100}% )`,
+          }
+        }
+      >
+        {React.Children.map(children, (child, index) => {
+          return React.cloneElement(child, { width: '100%' });
+        })}
+      </inner-content>
+      <div className="indicators">
+        <button
+          onClick={() => {
+            updateIndex(activeIndex - 1);
+          }}
+        >
+          Prev
+        </button>
+        <button
+          onClick={() => {
+            updateIndex(activeIndex + 1);
+          }}
+        >
+          Next
+        </button>
+      </div>
+    </motion-carousel>
+  );
+};
+
+export default MotionCarousel;
--- src/components/templates/TemplatesCarouselHero/TemplatesCarouselHero.jsx
+++ src/components/templates/TemplatesCarouselHero/TemplatesCarouselHero.jsx
@@ -1,32 +1,38 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './TemplatesCarouselHero.css';
-import TemplatesCarousel, {
+import MotionCarousel, {
   CarouselItem,
-} from '../../UI/organisms/TemplatesCarousel/TemplatesCarousel';
+} from '../../UI/organisms/MotionCarousel/MotionCarousel';
 import gaming from '../../../assets/images/f0c3c841c7c85883e0ceec610e446e81e5b84aa5e1c70891686ed3bdb98c7880.webp';
 import gaming1 from '../../../assets/images/wallpapersden.com_k-destiny-2_3840x2160.jpg';
 import social from '../../../assets/images/fashion-banner.webp';
 import CarouselText from '../../UI/molecules/CarouselText/CarouselText';
 import categories from '../../category_data';
+import { AnimatePresence } from 'framer-motion/dist/framer-motion';
+import { TemplatesContext } from '../../../Contexts/TemplatesPageContext';
 const TemplatesCarouselHero = () => {
+  const { activeIndex, setActiveIndex } = useContext(TemplatesContext);
   return (
     <section className="templates-carousel-hero">
       <fade-bg1 />
-      <TemplatesCarousel>
-        {categories.map((category, key) => (
-          <CarouselItem>
-            <item-container className="item-container">
-              <img src={category.image} className="cover-image" />
-              <box-shadow-overlay className="box-shadow-overlay" />
-              <CarouselText
-                header={category.title}
-                subtext={category.tagline}
-                description={category.description}
-              />
-            </item-container>
-          </CarouselItem>
-        ))}
-      </TemplatesCarousel>
+      <MotionCarousel>
+        {categories &&
+          categories
+            .filter((category) => category.id === activeIndex + 1)
+            .map((category, key) => (
+              <CarouselItem>
+                <item-container className="item-container">
+                  <img src={category.image} className="cover-image" />
+                  <box-shadow-overlay className="box-shadow-overlay" />
+                  <CarouselText
+                    header={category.title}
+                    subtext={category.tagline}
+                    description={category.description}
+                  />
+                </item-container>
+              </CarouselItem>
+            ))}
+      </MotionCarousel>
     </section>
   );
 };
