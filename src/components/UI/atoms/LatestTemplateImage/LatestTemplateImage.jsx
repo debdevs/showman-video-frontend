54c750e8e6c52f1db39c51ae3ff30a6d6e35215e
debdevs <debdevs97@gmail.com>
2022-11-03 00:24:59 -0400

    revise home page styling
    edited css values for components on home page to have better alignment

--- src/components/UI/atoms/LatestTemplateImage/LatestTemplateImage.jsx
+++ src/components/UI/atoms/LatestTemplateImage/LatestTemplateImage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './LatestTemplateImage.css';
 import template_image from '../../../../assets/images/wedding_image.webp';
-const LatestTemplateImage = () => {
+const LatestTemplateImage = ({ image_height, image_width, image_class }) => {
   return (
     <div className="latest-template-image">
       <div className="product_category_image_fx_container">
--- src/components/UI/atoms/ProfilePageTextInfo/ProfilePageTextInfo.jsx
+++ src/components/UI/atoms/ProfilePageTextInfo/ProfilePageTextInfo.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './ProfilePageTextInfo.css';
-const ProfilePageTextInfo = () => {
+const ProfilePageTextInfo = ({
+  header_text = 'Current Order',
+  subtext = 'Template Name',
+}) => {
   return (
     <div className="profile-info-text">
-      <h1 className="rumble-brave-font">Current Order:</h1>
-      <h2 className="istok-font">Stylish Media Social</h2>
+      <h1 className="rumble-brave-font">{header_text}:</h1>
+      <h2 className="istok-font">{subtext}</h2>
     </div>
   );
 };
