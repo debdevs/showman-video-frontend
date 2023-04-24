5fada5f49818f041ffefdd0a0ab93348daee489f
debdevs <debdevs97@gmail.com>
2023-04-24 19:08:15 -0400

    refactor carousel text

--- src/components/UI/molecules/CarouselText/CarouselText.jsx
+++ src/components/UI/molecules/CarouselText/CarouselText.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './CarouselText.css';
 const CarouselText = ({ header, subtext, description }) => {
   return (
-    <carousel-text>
+    <div className='carousel_text'>
       <h1 className="rumble-brave-font">{header}</h1>
       <h2 className="outfit">{subtext}</h2>
       <p className="outfit">{description}</p>
-    </carousel-text>
+    </div>
   );
 };
 
