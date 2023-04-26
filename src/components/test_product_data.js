dd18796bb9bf64aec6d100b5f885689afb5f8c17
debdevs <debdevs97@gmail.com>
2023-04-26 00:38:27 -0400

    add variables to TemplatesCategoryCard

--- src/components/test_product_data.js
+++ src/components/test_product_data.js
@@ -1,18 +1,21 @@
-import gaming_image from '../assets/images/wallpapersden.com_k-destiny-2_3840x2160.jpg';
+import gaming_image from '../assets/images/AnimatedTwitchOverlayTest.png';
 import social_image from '../assets/images/fashion-banner.webp';
-import wedding_image from '../assets/images/wedding_image_2.jpg';
-import real_estate_image from '../assets/images/real_estate_image_3.jpg';
+import wedding_image from '../assets/images/ElegeantWeddingPromo.png';
+import real_estate_image from '../assets/images/RealEstatePromo.png';
 import instagram_story from '../assets/images/instagram_story.png';
+import stomp_promo from '../assets/images/StompPromoScreenShot.png';
+import sleek_promo from '../assets/images/SleekProductPromo.png';
 import all_category from '../assets/images/all.jpg';
-
+import marketing_promo from '../assets/images/DigitalMarketingPromo.png';
+import black_friday_promo from '../assets/images/Black_Friday_Instagram_Promo.png';
 import React from 'react';
 
 const products_templates = [
   {
     id: 1,
-    title: 'Stellar Wedding Invite',
-    image: wedding_image,
-    tagline: 'Stellar Wedding Invite',
+    title: 'Stomp Typography Promo',
+    image: stomp_promo,
+    tagline: 'Stomp Typography Promo',
     description:
       'An amazing and professional wedding invitation video for your projects',
   },
@@ -42,7 +45,7 @@ const products_templates = [
   {
     id: 5,
     title: 'All Templates',
-    image: all_category,
+    image: marketing_promo,
     tagline: "Too many options? That's the best part",
     description:
       'Find any kind of template to make any kind of video. Place your edits & make your mark',
@@ -50,7 +53,7 @@ const products_templates = [
   {
     id: 6,
     title: 'All Templates',
-    image: all_category,
+    image: sleek_promo,
     tagline: "Too many options? That's the best part",
     description:
       'Find any kind of template to make any kind of video. Place your edits & make your mark',
@@ -59,7 +62,7 @@ const products_templates = [
   {
     id: 7,
     title: 'All Templates',
-    image: all_category,
+    image: black_friday_promo,
     tagline: "Too many options? That's the best part",
     description:
       'Find any kind of template to make any kind of video. Place your edits & make your mark',
