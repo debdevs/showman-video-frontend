e3fd59661411acf027c4b2ce68fa1f818528871f
Deborah <debdevs97@gmail.com>
2023-04-30 15:44:45 -0400

    css

--- src/components/templates/Footer/Footer.jsx
+++ src/components/templates/Footer/Footer.jsx
@@ -5,47 +5,45 @@ import EmailBox from '../../UI/atoms/EmailBox/EmailBox';
 import { RiFacebookFill } from 'react-icons/all';
 import { RiInstagramLine } from 'react-icons/all';
 import { RiTwitterFill } from 'react-icons/all';
+import {ReactComponent as FacebookButton} from '../../../assets/images/FacebookButton.svg'
+import {ReactComponent as LinkedInButton} from '../../../assets/images/LinkedInButton.svg'
+import {ReactComponent as EmailButton} from '../../../assets/images/EmailButton.svg'
+import showman_logo from '../../../assets/images/Showman_V02_logodesat.png'
 
 const Footer = () => {
   return (
-    <footer-parent>
-      <footer-content-container>
-        <footer-left-items>
-          <img
-            src={require('../../../assets/images/logo.png').default}
-            alt=""
-          />
-          <social-row>
-            <icon-box>
-              <RiFacebookFill className="social-icon" />
-            </icon-box>
-            <icon-box>
-              <RiInstagramLine className="social-icon" />
-            </icon-box>
-            <icon-box>
-              <RiTwitterFill className="social-icon" />
-            </icon-box>
-          </social-row>
-        </footer-left-items>
+    <div className='footer_parent_container'>
+<div className='footer_parent'>
+  <div className='footer_contents'>
+    <div className='footer_top_items'>
+      <div className='logo_wt_text'>
+      <img src = {showman_logo} className='footer_logo'/>
+        <h1 className='footer_text'>
+          Your destination for media pizazz
+        </h1>
+      </div>
+      <div className='footer_items_row'>
+      <p>Home</p>
+      <p>About</p>
+      <p>Templates</p>
+      <p>Licensing</p>
+      <p>Etsy</p>
+      </div>
+      
+    </div>
+<div className='footer_bottom_items'>
+<div className='footer_button_row'>
+      <FacebookButton className = 'footer_icon'/>
+      <LinkedInButton className = 'footer_icon'/>
+      <EmailButton className = 'footer_icon'/>
 
-        <footer-center-items>
-          <ul className="center-links">
-            <li className="footer-link outfit">Home</li>
-            <li className="footer-link outfit">Templates</li>
-            <li className="footer-link outfit">About</li>
-            <li className="footer-link outfit">Contact</li>
-          </ul>
-          <p className="outfit footer-paragraph">
-            Find the most stylish media templates here at Showman. No
-            subscription required. Customizable and Social media ready.
-          </p>
-        </footer-center-items>
+    </div>
+</div>
 
-        <footer-right-items>
-          <EmailBox />
-        </footer-right-items>
-      </footer-content-container>
-    </footer-parent>
+
+  </div>
+</div>
+</div>
   );
 };
 
