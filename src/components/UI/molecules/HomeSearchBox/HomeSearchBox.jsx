42430423601d32b4dafa69bf0ddc7fb38d37cae3
Deborah <debdevs97@gmail.com>
2023-10-18 23:56:07 -0400

    update home search box animation

--- src/components/UI/molecules/HomeSearchBox/HomeSearchBox.jsx
+++ src/components/UI/molecules/HomeSearchBox/HomeSearchBox.jsx
@@ -1,66 +1,59 @@
-import React from 'react';
-import BasicButton from '../../atoms/BasicButton/BasicButton';
-import '../../../../App.css';
-import './HomeSearchBox.css';
-import { useState } from 'react';
-import { GoSearch } from 'react-icons/all';
-import { ReactComponent as Rhombus } from '../../../../assets/images/button-rhombus.svg';
+import React from "react";
+import BasicButton from "../../atoms/BasicButton/BasicButton";
+import "../../../../App.css";
+import "./HomeSearchBox.css";
+import { useState } from "react";
+import { GoSearch } from "react-icons/all";
+import { ReactComponent as Rhombus } from "../../../../assets/images/button-rhombus.svg";
 const HomeSearchBox = () => {
   const [activeItem, setActiveItem] = useState(true);
   return (
-    <div className='box_parent'>
-      <div className='parent-container'>
-      <div className='input_container'>
-      <div
-        className={
-          activeItem === false ? 'input-parent' : 'input-parent-active'
-        }
-      >
-        <div
-          onFocus={() => {
-            setActiveItem(true);
-          }}
-          onBlur={() => {
-            setActiveItem(false);
-          }}
-          className={
-            activeItem === false ? 'search-input' : 'search-input-active'
-          }
-        >
-          <input className='outfit absolute_input'
-                 onFocus={() => {
+    <div className="box_parent">
+      <div className="parent-container">
+        <div className="input_container">
+          <div
+            className={
+              activeItem === false ? "input-parent" : "input-parent-active"
+            }
+          >
+            <div
+              onFocus={() => {
+                setActiveItem(true);
+              }}
+              onBlur={() => {
+                setActiveItem(false);
+              }}
+              className={
+                activeItem === false ? "search-input" : "search-input-active"
+              }
+            >
+              <input
+                className="outfit absolute_input"
+                onFocus={() => {
                   setActiveItem(true);
                 }}
                 onBlur={() => {
                   setActiveItem(false);
                 }}
+              ></input>
+            <GoSearch
+        className={activeItem === false ? "search-icon" : "search-icon-active"}
+      />
+            </div>
 
-          >
-          
-          </input>
-
-
+          </div>
         </div>
-
       </div>
+      <button
+        className={
+          activeItem === false ? "search-button" : "search-button-active"
+        }
+      >
+        <Rhombus className="logo" />
+        <h3 className="outfit">DEMO</h3>
+      </button>
 
-
-      </div>
-      <GoSearch
-          className={
-            activeItem === false ? 'search-icon' : 'search-icon-active'
-          }
-        />
-        </div>
-        <button className={
-            activeItem === false ? 'search-button' : 'search-button-active'
-          }><Rhombus className="logo" /> 
-          <h3 className="outfit">
-          DEMO
-          </h3>
-        </button>
     </div>
-    
   );
 };
 
